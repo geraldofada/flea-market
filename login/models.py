18aa39b713aa3458ce58bca1a6e5b8f7146b4a52
Geraldo Fada <geraldofada.neto@gmail.com>
2021-08-01 16:11:23 -0300

    base models created

--- login/models.py
+++ login/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    cpf = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(unique=True)
+    nickname = models.CharField(max_length=30, unique=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nickname
--- product/models.py
+++ product/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from category.models import Category
+from login.models import User
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    description = models.TextField(blank=True)
+    slug = models.SlugField(max_length=100)
+    image = models.CharField(max_length=150, blank=True)
+    quantity = models.IntegerField(default=1)
+    available = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    category = models.ManyToManyField(Category)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Question(models.Model):
+    text = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
+
+class Awnser(models.Model):
+    text = models.TextField(blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    question = models.OneToOneField(Question, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.text
