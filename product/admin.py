84a4066c33316f668615dbe17f2afefb8daa5b55
Geraldo Fada <geraldofada.neto@gmail.com>
2021-08-01 18:37:12 -0300

    added more models to admin

--- product/admin.py
+++ product/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from product import models
 from product.models import Product
 
 class ProductAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
 
 
-admin.site.register(Product, ProductAdmin)
+admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.Awnser)
+admin.site.register(models.Question)
