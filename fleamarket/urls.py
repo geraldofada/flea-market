5a83d383d2971e91d131081ec25d00c3dc9d0536
Geraldo Fada <geraldofada.neto@gmail.com>
2021-08-01 18:36:51 -0300

    new product page url

--- fleamarket/urls.py
+++ fleamarket/urls.py
@@ -6,6 +6,7 @@ from fleamarket import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('', include('login.urls')),
+    path('product/', include('product.urls')),
     path('category/', include('category.urls')),
     path('admin/', admin.site.urls),
 ]
