14d113f78953487931b0478ca2d6cd63812f22c2
Geraldo Fada <geraldofada.neto@gmail.com>
2021-08-01 17:59:03 -0300

    index now showing all products

--- fleamarket/views.py
+++ fleamarket/views.py
@@ -1,10 +1,14 @@
+from product.models import Product
 from django.shortcuts import render
 from category.models import Category
+from product.models import Product
 
 def index(request):
     categories = Category.objects.all().order_by('name')
+    products = Product.objects.all()
     context = {
-        'categories': categories
+        'categories': categories,
+        'products': products
     }
 
     return render(request, 'index.html', context)
