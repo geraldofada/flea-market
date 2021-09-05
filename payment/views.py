174a269657dc148f52e2785933da73758e6e86be
Geraldo Fada <geraldofada.neto@gmail.com>
2021-09-05 19:33:41 -0300

    sale history added

--- payment/views.py
+++ payment/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from product.models import Product
 from category.models import Category
-from payment.models import Purchase
+from payment.models import Purchase, Sale
 from django.urls import reverse
 
 
@@ -30,18 +30,23 @@ def purchase_show(request):
 @login_required
 def sale_show(request):
     categories = Category.objects.all()
-    purchase = Purchase.objects.all().filter(owner=request.user).order_by('-created_at')
+    sale = Sale.objects.all().filter(owner=request.user).order_by('-created_at')
 
     page = request.GET.get('page')
-    paginator = Paginator(purchase, 6)
-    purchase_obj = paginator.get_page(page)
+    paginator = Paginator(sale, 6)
+    sale_obj = paginator.get_page(page)
+
+    total_per_page = 0
+    for prod in sale_obj:
+        total_per_page += prod.product_price
 
     context = {
         'categories': categories,
-        'purchase': purchase_obj
+        'sale': sale_obj,
+        'total_per_page': total_per_page
     }
 
-    return render(request, 'payment/purchase_show.html', context)
+    return render(request, 'payment/sale_show.html', context)
 
 @login_required
 def buy(request, id):
@@ -58,6 +63,15 @@ def buy(request, id):
                 product_small_description=product.small_description
             )
 
+            sale = Sale(
+                owner=product.owner,
+                product=product,
+                product_name=product.name,
+                product_price=product.price,
+                product_small_description=product.small_description
+            )
+
+            sale.save()
             product.save()
             purchase.save()
 
@@ -85,11 +99,20 @@ def close_cart(request):
                     product_small_description=product.small_description
                 )
 
+                sale = Sale(
+                    owner=product.owner,
+                    product=product,
+                    product_name=product.name,
+                    product_price=product.price,
+                    product_small_description=product.small_description
+                )
+
                 cart.products.remove(product)
 
                 cart.save()
                 product.save()
                 purchase.save()
+                sale.save()
 
         if purchase_ok:
             messages.success(request, 'Compra finalizada com sucesso!')
