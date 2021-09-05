d0e2a1e12743519fdf0f49fbb59e05433b11c835
Geraldo Fada <geraldofada.neto@gmail.com>
2021-09-05 17:48:50 -0300

    purchase model

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,20 @@
+import uuid
 from django.db import models
+from django.db.models.deletion import CASCADE, SET_NULL
+from users.models import User
+from product.models import Product
 
-# Create your models here.
+
+class Purchase(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    owner = models.ForeignKey(User, on_delete=CASCADE)
+    product = models.ForeignKey(Product, on_delete=SET_NULL, null=True)
+    product_name = models.CharField(max_length=100)
+    product_price = models.DecimalField(max_digits=12, decimal_places=2, default=1)
+
+    def __str__(self):
+        return f'{self.owner.username}_{self.product_name}'
